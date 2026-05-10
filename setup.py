f7560f24f8ba3173464497e82b0e8c8aa22d20a1
Akash <tajuar.akash@gmail.com>
2026-05-10 09:05:39 +0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Akash",
+    author_email="tajuar.akash@gmail.com",
+    packages=find_packages()
+)
